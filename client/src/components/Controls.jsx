895f88233b307bbe232c4c3b2423ad581dbd7bba
Hebatullah Dwairi <hebamahmoud8843@gmail.com>
2025-02-06 20:30:01 +0300

    implement note detection

--- client/src/components/Controls.jsx
+++ client/src/components/Controls.jsx
@@ -1,5 +1,5 @@
 import melody from "../services/melody";
-
+import noteDetection from '../services/noteDetection';
 const Controls = ({onPlay, onGenerate}) => {
     return (
       <div className="bg-secondary dark:bg-secondary-d p-4 m-6 flex justify-center gap-4 rounded-lg font-bold">
@@ -9,7 +9,7 @@ const Controls = ({onPlay, onGenerate}) => {
         <button className="btn" onClick={onPlay}>
           Play
         </button>
-        <button className="btn">
+        <button className="btn" onClick={() => noteDetection.start()}>
           Record
         </button>
       </div>
