ee3bf10f271a9230d935ebcf150afce2cf7e55de
Hebatullah Dwairi <hebamahmoud8843@gmail.com>
2025-03-12 23:31:31 +0300

    login page layout

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,8 +1,19 @@
 const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+  }
   return(
-    <>
-      <div>Login page</div>
-    </>
+    <div className="flex flex-1 justify-center items-center">
+      <div className="flex flex-col justify-center items-center gap-10 border-2 border-primary/40 rounded-md p-5">
+        <p className="text-xl font-bold">Log in</p>
+        <form onSubmit={handleLogin} className="flex flex-col gap-5">
+          <input type="text" placeholder="Username" className="inp"/>
+          <input type="password" placeholder="Password" className="inp"/>
+          <button className="btn" type="submit">login</button>
+        </form>
+        <p>Don&apos;t have an account? <span className="text-primary hover:underline cursor-pointer hover:text-accent">Register</span></p>
+      </div>
+    </div>
   );
 }
 
