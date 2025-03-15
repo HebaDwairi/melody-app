5219fcf9fcd37b02b39d2a1ec87869795a02907f
Hebatullah Dwairi <hebamahmoud8843@gmail.com>
2025-03-16 00:19:57 +0300

    profile page

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -1,8 +1,27 @@
+import { useAuth } from "../contexts/authContext";
+
 const Profile = () => {
+
+  const { user } = useAuth();
+  if(!user) {
+    return <div>Loading...</div>;
+  }
   return(
-    <>
-      <div>profile page</div>
-    </>
+    <div className="text-lg ">
+      <div className="sm:w-100 border-2 border-primary/40 rounded-md p-5 m-6 bg-primary dark:bg-primary-d text-background ">
+        <p className="font-bold mb-5 text-xl">User info</p>
+        <p>Username:  {user.username}</p>
+        <p>Name:  {user.name}</p>
+      </div>
+      <div className="sm:w-100 border-2 border-primary/40 rounded-md p-5 m-6 bg-primary dark:bg-primary-d text-background">
+        <p className="font-bold mb-5 text-xl">Edit your info</p>
+      </div>
+      <div className="sm:w-100 border-2 border-primary/40 rounded-md p-5 m-6 bg-primary dark:bg-primary-d text-background">
+        <p className="font-bold mb-5 text-xl">Account actions</p>
+        <p className="text-rose-300">Reset Data</p>
+        <p className="text-rose-300">Delete Account</p>
+      </div>
+    </div>
   );
 }
 
