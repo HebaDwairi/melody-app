206c22160b9c65e42a16792f3bda33e926b23704
Hebatullah Dwairi <hebamahmoud8843@gmail.com>
2025-03-17 15:59:20 +0300

    uibuild

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('connection with mongodb failed ', err.message);
   });
 
-
+app.use(express.static('dist'))
 app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
